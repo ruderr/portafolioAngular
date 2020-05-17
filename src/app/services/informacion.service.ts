ebb4514b0ba4fb129756dcea2e369aa79374a191
ruderr <ruderr@gmail.com>
2020-05-16 23:50:38 -0300

    Servicio firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -9,14 +9,31 @@ export class InformacionService {
 
   info:any = {};
   cargada:boolean = false;
+  cargada_sobre_nosotros:boolean = false;
+  equipo:any = [];
   
   constructor(public http:HttpClient) {
 
+    this.carga_info();
+    this.carga_sobre_nosotros();
+
+   }
+
+   public carga_info(){
     this.http.get("assets/data/info.pagina.json")
               .subscribe(data => {
-                console.log(data);
+                //console.log(data);
                 this.cargada = true;
                 this.info = data;
+              })
+   }
+
+   public carga_sobre_nosotros(){
+    this.http.get("https://portfolio-7266b.firebaseio.com/equipo.json")
+              .subscribe(data => {
+                console.log(data);
+                this.cargada_sobre_nosotros = true;
+                this.equipo = data;
               })
    }
 }
